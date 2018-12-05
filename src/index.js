2fa48d61a5e2528a40176b33bff57c556464d56b
artem-iliasov <iliasov.artem.27@gmail.com>
2018-12-05 19:28:05 +0300

    greeting function

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const greeting = () => {
+    console.log('Welcome to the Brain Games!\n');
+    const playerName = readlineSync.question('May i have your name?\n', {
+        defaultInput: 'anonim'
+    });
+    console.log(`Hello, ${playerName}!\n`)
+};
+
+export default greeting;
