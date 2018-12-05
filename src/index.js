816f23977e8fbfafb03806b1a03a13b85f5f3f94
artem-iliasov <iliasov.artem.27@gmail.com>
2018-12-05 22:14:46 +0300

    after travis linter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 
 const greeting = () => {
   console.log('Welcome to the Brain Games!\n');
-    const playerName = readlineSync.question('May i have your name?\n', {
-      defaultInput: 'anonim',
+  const playerName = readlineSync.question('May i have your name?\n', {
+    defaultInput: 'anonim',
   });
   console.log(`Hello, ${playerName}!\n`);
 };
