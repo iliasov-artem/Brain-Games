583c529b92a1965720b2c57eb1cc17f8b930c3b0
artem-iliasov <iliasov.artem.27@gmail.com>
2018-12-07 19:09:59 +0300

    game engine

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  console.log('Welcome to the Brain Games!\n');
-  const playerName = readlineSync.question('May i have your name?\n', {
-    defaultInput: 'anonim',
-  });
+const playGame = (game, makeGame) => {
+  console.log('Welcome to the Brain Games!');
+  console.log(game);
+  const playerName = readlineSync.question('May I have your name? ', { defaultInput: 'anonim' });
   console.log(`Hello, ${playerName}!\n`);
+  for (let atempts = 3; atempts > 0; atempts -= 1) {
+    const gameState = makeGame();
+    const { question, rightAnswer } = gameState;
+    const playerAnswer = readlineSync.question(`${question}\n`);
+    console.log(`Your answer: ${playerAnswer}`);
+    if (playerAnswer !== rightAnswer) {
+      console.log(`'${playerAnswer} is wrong answer ;(. Correct answer was '${rightAnswer}'`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+  console.log(`Congratulations, ${playerName}`);
 };
 
-export default greeting;
+export default playGame;
