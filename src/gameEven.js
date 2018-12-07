329cc8ab3eb23d95596eeca8096accaa4b24985f
iliasov-artem <45028414+iliasov-artem@users.noreply.github.com>
2018-12-07 08:07:56 +0300

    Update gameEven

--- src/gameEven.js
+++ src/gameEven.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const getRandomNum = (min, max) => Math.floor(min + Math.random() * (max + 1 - min));
+const getRandomNum = (min = 1, max = 100) => Math.floor(min + Math.random() * (max + 1 - min));
+const isEven = num => num % 2 ? false : true;
 
 export default () => {
   console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
@@ -8,18 +9,17 @@ export default () => {
     defaultInput: 'anonim',
   });
   console.log(`Hello, ${playerName}!\n`);
-  let atempts = 3;
-  while (atempts) {
-    const num = getRandomNum(1, 100);
-    const answer = readlineSync.question(`Question: ${num}\n`);
-    const rightAnswer = num % 2 ? 'no' : 'yes';
+  for (let atempts = 0; atempts < 3; atempts += 1) {
+    const question = getRandomNum();
+    const answer = readlineSync.question(`Question: ${question}\n`);
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
     console.log(`Your answer: ${answer}`);
     if (answer !== rightAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${playerName}`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${playerName}!`);
       return;
     }
     console.log('Correct!');
-    atempts -= 1;
   }
   console.log(`Congratulations, ${playerName}!`);
 };
