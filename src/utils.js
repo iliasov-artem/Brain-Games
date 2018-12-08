7df81887fb5ddade284b4ee4a57c64c71048a159
artem-iliasov <iliasov.artem.27@gmail.com>
2018-12-08 13:53:54 +0300

    update

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,4 @@
-const getRandomNum = (min = 1, max = 100) => {
+export const getRandomNum = (min = 1, max = 100) => {
   const num = Math.floor(min + Math.random() * (max + 1 - min));
   return num;
 };
-
-export default getRandomNum;
