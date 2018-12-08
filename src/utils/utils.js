ff0a541d2bf11a9ac608a02582eaafd8bbb77d62
artem-iliasov <iliasov.artem.27@gmail.com>
2018-12-08 13:26:08 +0300

    delete folder

--- src/utils/utils.js
+++
@@ -1,6 +0,0 @@
-export const getRandomNum = (min = 1, max = 100) => {
-  const num = Math.floor(min + Math.random() * (max + 1 - min));
-  return num;
-};
-
-export const isEven = num => !(num % 2);
